b9a1a44f023ce344a720e82a7aafe8d21e1ca860
yayoimizuha <poko.peko.tomo@icloud.com>
2022-03-10 15:30:50 +0900

    update

--- main.py
+++ main.py
@@ -5,38 +5,37 @@ import tensorflow
 
 temp = subprocess.run(['nvidia-smi ', '--query-gpu=temperature.gpu', '--format=csv,noheader'], stdout=subprocess.PIPE)
 
+print(tensorflow.config.experimental.list_physical_devices("GPU"))
+print(tensorflow.config.experimental.list_physical_devices("CPU"))
 print(int(temp.stdout.split()[-1]))
 
 numpy.random.seed(0)
 
 
-def run():
+def run(device):
     random_base = numpy.random.rand(10000, 10000)
-    cpu_inved = numpy.linalg.inv(random_base)
+    # cpu_inved = numpy.linalg.inv(random_base)
     # print(random_base.tolist())
 
-    print(tensorflow.config.experimental.list_physical_devices("GPU"))
-    print(tensorflow.config.experimental.list_physical_devices("CPU"))
+    with tensorflow.device(device):
+        base_tensor = tensorflow.constant(random_base.tolist())
 
-    base_tensor = tensorflow.constant(random_base.tolist())
+        inved = tensorflow.linalg.inv(base_tensor)
 
-    inved = tensorflow.linalg.inv(base_tensor)
+        INVxBASE = tensorflow.matmul(inved, base_tensor)
 
-    INVxBASE = tensorflow.matmul(inved, base_tensor)
+        Error = tensorflow.subtract(INVxBASE, tensorflow.math.round(INVxBASE)).numpy()
 
-    Error = tensorflow.subtract(INVxBASE, tensorflow.math.round(INVxBASE)).numpy()
-
-
-    print(Error.sum())
+        print(device, end='\t')
+        print(Error.sum())
 
 
 for i in range(20):
-    tensorflow.device('CPU:0')
-    run()
+    pass
+    # run('CPU:0')
 
 print('\n\n\n\n\n\nGPU\n\n\n\n\n\n')
 
 
 for i in range(20):
-    tensorflow.device('/physical_device:GPU:0')
-    run()
+    run('GPU:0')
